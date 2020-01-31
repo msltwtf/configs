85203cfedb0c76716ff5a554d3d5f3ed8a5c2d89
Marius Leonhardt <marius.leonhardt@interred.de>
2020-01-31 11:08:11 +0100

    neovim - create path bevor using them

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.local/share/nvim/site/autoload
-mkdir -p ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp init.vim ~/.config/nvim/init.vim
