820093f8c82f17de429eeaf3ab1e051c0a8f022d
Marius Leonhardt <marius.leonhardt@interred.de>
2020-01-31 11:06:30 +0100

    FUQ

--- vim/install-plugins.sh
+++ vim/install-plugins.sh
@@ -16,6 +16,12 @@ for plugin in $(cat $PWD/plugins) ; do
 		echo -n "Installing $plugin "
 		git clone https://github.com/${plugin} -q
 		echo "ok"
+  elif [[ -d $HOME/.vim/pack/plugins/start/$plugdir ]] ; then
+    echo -n "Plugin $plugin already installed! Remove [y/N] "
+    read optRemove
+    if [[ $optRemove == 'y' ]] ; then
+      rm "$HOME/.vim/pack/plugins/start/$plugdir -rf"
+    fi
 	fi
 done
 		
