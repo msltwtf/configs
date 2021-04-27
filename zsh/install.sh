08548064ad64549c47de0f2c54e7744a632f3ad1
Marius Leonhardt <info@marius-leonhardt.de>
2021-04-27 23:00:34 +0200

    zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,13 +5,17 @@ if [[ ! -d $HOME/.oh-my-zsh ]] ; then
   curl -L install.ohmyz.sh | zsh
 fi
 
-# Install powerlevel9k
+# Install powerlevel10k
 if [[ ! -d $HOME/.oh-my-zsh/themes/powerlevel9k ]] ; then
   git clone https://github.com/powerlevel9k/powerlevel9k $HOME/.oh-my-zsh/themes/powerlevel9k/
+fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]] ; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
 ln -sf $HOME/git/configs/zsh/zshrc $HOME/.zshrc
 ln -sf $HOME/git/configs/zsh/aliases $HOME/.aliases
+ln -sf $HOME/git/configs/zsh/p10k.zsh $HOME/.p10k.zsh
 
 # Ask for fonts
 echo "Should the powerline patched fonts be installed? [Y]es|[N]o"
