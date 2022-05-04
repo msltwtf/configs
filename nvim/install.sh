7d70f8ff42958f822d8dc42f39f6e231de6621cc
Marius Leonhardt <info@marius-leonhardt.de>
2022-05-05 01:16:43 +0200

    Restored install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,8 +11,20 @@ python3 -m venv ~/.config/nvim/venv
 
 source ~/.config/nvim/venv/bin/activate
 
-pip3 install -U neovim
+pip3 install -U flake8 pylint yapf neovim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
+
+if [[ ! -e /usr/bin/node ]] ; then
+  echo "NodeJS is not installed!"
+fi
+
+if [[ ! -e /usr/bin/yarn ]] ; then
+  echo "Yarn is not installed!"
+fi
+
+if [[ ! -e /usr/bin/npm ]] ; then
+  echo "NPM is not installed!"
+fi
