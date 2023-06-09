17607b76499bf712f8c5597682f795683615ecf1
Marius Leonhardt <info@marius-leonhardt.de>
2023-06-09 02:27:58 +0200

    add working copilot config

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -1,3 +1,5 @@
+local cmp = require "cmp"
+
 local plugins = {
   {
     "tpope/vim-fugitive",
@@ -98,16 +100,55 @@ local plugins = {
     "lervag/vimtex",
     ft = "tex",
     lazy = false
-  }
-  --[[{
-    "github/copilot.vim",
-    lazy = false
-  }
-  --]]
   },
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    opts = function ()
+      return require "custom.configs.copilot"
+    end,
+    config = function(_, opts)
+      require("copilot").setup(opts)
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      }
+
+      M.mapping["<C-j>"] = cmp.mapping(function(_fallback)
+        cmp.mapping.abort()
+        require("copilot.suggestion").accept_line()
+      end, {
+          "i",
+          "s",
+        })
+
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates  = require('crates')
+      crates.setup(opts)
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+      crates.show()
+      require("core.utils").load_mappings("crates")
+    end,
   },
 }
 
