50160e38eefdf291356194e62faf8beedbcbd011
Marius Leonhardt <info@marius-leonhardt.de>
2023-06-13 01:45:26 +0200

    new tmux conf

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-if [[ ! -d ${HOME}/.tmux ]] ; then
-  git clone https://github.com/gpakosz/.tmux $HOME/.tmux
-fi
-ln -s $HOME/git/configs/tmux/tmux.conf.local $HOME/.tmux.conf.local
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
